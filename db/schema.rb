91be838792ff4585fb13c528b1994c055b564dfd
Gio Cielo <gccielo@gmail.com>
2014-03-25 14:26:12 -0700

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20140214211850) do
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20140214211850) do
     t.datetime "updated_at"
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20140214211850) do
     t.datetime "updated_at"
   end
 
-  add_index "contacts", ["company_id"], name: "index_contacts_on_company_id", using: :btree
+  add_index "contacts", ["company_id"], name: "index_contacts_on_company_id"
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -88,8 +88,8 @@ ActiveRecord::Schema.define(version: 20140214211850) do
     t.datetime "updated_at"
   end
 
-  add_index "opportunities", ["contact_id"], name: "index_opportunities_on_contact_id", using: :btree
-  add_index "opportunities", ["event_id"], name: "index_opportunities_on_event_id", using: :btree
+  add_index "opportunities", ["contact_id"], name: "index_opportunities_on_contact_id"
+  add_index "opportunities", ["event_id"], name: "index_opportunities_on_event_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
